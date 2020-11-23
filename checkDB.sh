83df858c2642d539f8a948ea7e49141e8ac9e18c
JorgeOsorio97 <jorge.valdez.osorio@gmail.com>
2020-11-22 22:43:00 -0600

    paldui3

--- checkDB.sh
+++ checkDB.sh
@@ -8,8 +8,8 @@ then
 	# echo "BASE todo cool <br />" >> /var/www/html/index.html
 	python3 /home/ulises/exito.py
 else
-	echo "Base de datos caida"
-	date >> /var/www/html/index.html
-	echo "Base de datos caida <br />" >> /var/www/html/index.html
+	# echo "Base de datos caida"
+	# date >> /var/www/html/index.html
+	# echo "Base de datos caida <br />" >> /var/www/html/index.html
 	python3 /home/ulises/error.py
 fi
