fb6d8b41baeae2523ef5e9639807341861a4a90b
JorgeOsorio97 <jorge.valdez.osorio@gmail.com>
2020-11-22 21:50:21 -0600

    agregar fecha a sh

--- checkDB.sh
+++ checkDB.sh
@@ -1,9 +1,13 @@
 systemctl status mysqld | grep "running" >> /dev/null 
+
+date
 if [ $? -eq 0 ] 
 then 
 	echo "BASE todo cool"
+	date >> /var/www/html/index.html
 	echo "BASE todo cool <br />" >> /var/www/html/index.html
 else
 	echo "Base de datos caida"
+	date >> /var/www/html/index.html
 	echo "Base de datos caida <br />" >> /var/www/html/index.html
 fi
