8a63854ceb29203ed0b0a038a64d04e6f9b3452f
JorgeOsorio97 <jorge.valdez.osorio@gmail.com>
2020-11-22 22:50:39 -0600

    quitar dui

--- checkDB.sh
+++ checkDB.sh
@@ -3,13 +3,13 @@ systemctl status mysqld | grep "running" >> /dev/null
 date
 if [ $? -eq 0 ] 
 then 
-	# echo "BASE todo cool"
-	# date >> /var/www/html/index.html
-	# echo "BASE todo cool <br />" >> /var/www/html/index.html
+	echo "BASE todo cool"
+	date >> /var/www/html/index.html
+	echo "BASE todo cool <br />" >> /var/www/html/index.html
 	python3 /home/ulises/exito.py
 else
-	# echo "Base de datos caida"
-	# date >> /var/www/html/index.html
-	# echo "Base de datos caida <br />" >> /var/www/html/index.html
+	echo "Base de datos caida"
+	date >> /var/www/html/index.html
+	echo "Base de datos caida <br />" >> /var/www/html/index.html
 	python3 /home/ulises/error.py
 fi
--- error.py
+++ error.py
@@ -1,32 +1,20 @@
-import requests
-import json
+
 from datetime import datetime as dt
-# from utils import send_email, send_sms
 # Manda correo de error
 
-# BODY_TEXT = ("ALERTA!!!!!!\r\n"
-#             "Tu base de datos se ha caido"
-#             )
+BODY_TEXT = ("ALERTA!!!!!!\r\n"
+            "Tu base de datos se ha caido"
+            )
 
-# BODY_HTML = """<html>
-#                 <head></head>
-#                 <body>
-#                 <h1>ALERTA!!!!!!</h1>
-#                 <p>Tu base de datos se ha caido</p>
-#                 </body>
-#             </html>
-#             """
-# send_email("jorge.valdez.osorio@gmail.com", "CheckDB", BODY_TEXT, BODY_HTML)
-# send_sms("Alerta en base de datos")
+BODY_HTML = """<html>
+                <head></head>
+                <body>
+                <h1>ALERTA!!!!!!</h1>
+                <p>Tu base de datos se ha caido {}</p>
+                </body>
+            </html>
+            """.format(dt.now())
+send_email("jorge.valdez.osorio@gmail.com", "CheckDB", BODY_TEXT, BODY_HTML)
+send_sms("Alerta en base de datos {}".format(dt.now()))
 
-# base_url="http://64acdb529bb1.ngrok.io"
 
-payload = {
-    "status": "error",
-    "date": str(dt.now())
-}
-headers = {
-  'Content-Type': 'application/json'
-}
-response = requests.request("POST", base_url + "/status", headers=headers, data=json.dumps(payload))
-print(response)
