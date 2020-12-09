8b627a6d650eb523379728e9bef2509e12df1c4c
JorgeOsorio97 <jorge.valdez.osorio@gmail.com>
2020-12-08 18:19:37 -0600

    agregar archivos sh apra cronjobs

--- backupDB/backup.py
+++ backupDB/backup.py
@@ -1,4 +1,3 @@
-import subprocess
 import os
 from datetime import datetime as dt
 
@@ -9,13 +8,11 @@ from util_db import WEEKDAYS
 weekday = dt.today().weekday()
 
 dbs =  pd.read_csv('data/{}.csv'.format(WEEKDAYS[weekday]))
-dbs =  {i[1]['db'] for i in dbs.iterrows()}
+dbs =  [i[1]['db'] for i in dbs.iterrows()]
 dump_cmd = 'mysqldump --password=1234567890 {db}  > /home/jorge/backups/{db}/backup_`date  +"%Y-%m-%d_%H:%M"`.sql'
 
 for db in dbs:
     temp = dump_cmd.format(db=db)
     print(temp)
-    # process = subprocess.run(temp
-    # print(process)
-    # output, error = process.communicate()
-    os.system(temp)
+    resu = os.system(temp)
+    print(res)
