e4a6311509a30ab803a5bffef70869e258381b2f
JorgeOsorio97 <jorge.valdez.osorio@gmail.com>
2021-01-17 23:16:15 -0600

    Add replication models 1
    parejas_replication model
    anillo3_replication model

---
+++ replicationDB/anillo3_replica/clean_volumes.sh
@@ -0,0 +1,6 @@
+rm -r -f ./hybrid1/data/*
+rm -r -f ./hybrid2/data/*
+rm -r -f ./hybrid3/data/*
+rm -r -f ./repl1/data/*
+rm -r -f ./repl2/data/*
+rm -r -f ./repl3/data/*
---
+++ replicationDB/pareja_replica/clean_volumes.sh
@@ -0,0 +1,4 @@
+rm -r -f ./hybrid1/data/*
+rm -r -f ./hybrid2/data/*
+rm -r -f ./repl1/data/*
+rm -r -f ./repl2/data/*
