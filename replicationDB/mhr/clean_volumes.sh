d277677b58927d3b6e09f255f63d4f1ad9bd706e
JorgeOsorio97 <jorge.valdez.osorio@gmail.com>
2021-01-18 00:40:00 -0600

    add mhr model

---
+++ replicationDB/mhr/clean_volumes.sh
@@ -0,0 +1,3 @@
+rm -r -f ./master1/data/*
+rm -r -f ./hybrid1/data/*
+rm -r -f ./repl1/data/*
---
+++ replicationDB/mhr/permissioncnf.sh
@@ -0,0 +1,3 @@
+chmod 0444 ./master1/mysqlconf/my.cnf
+chmod 0444 ./hybrid1/mysqlconf/my.cnf
+chmod 0444 ./repl1/mysqlconf/my.cnf
