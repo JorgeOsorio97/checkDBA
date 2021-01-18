1889848505f4234350e5e3465faf6e01f6f6e276
JorgeOsorio97 <jorge.valdez.osorio@gmail.com>
2021-01-18 00:19:06 -0600

    permissions fix in cnf files for anillo3

---
+++ replicationDB/anillo3_replica/permissioncnf.sh
@@ -0,0 +1,6 @@
+chmod 0444 ./hybrid1/mysqlconf/my.cnf
+chmod 0444 ./hybrid2/mysqlconf/my.cnf
+chmod 0444 ./hybrid3/mysqlconf/my.cnf
+chmod 0444 ./repl1/mysqlconf/my.cnf
+chmod 0444 ./repl2/mysqlconf/my.cnf
+chmod 0444 ./repl3/mysqlconf/my.cnf
